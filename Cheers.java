0f37ad9110ef9b475efa75a8b4b063c647e0647c
Ran Levy <Ran.levy02@post.runi.ac.il>
2025-11-07 12:07:03 +0200

    fixedd

--- Cheers.java
+++ Cheers.java
@@ -6,7 +6,7 @@ public class Cheers {
             String nik = "AEFHILMNORSX";
             for(int i = 0 ; i < word.length() ; i++ ){
                 if(nik.indexOf(word.charAt(i))==-1){
-                  System.out.println("Give me a " + word.charAt(i) +": " + word.charAt(i) +"!");
+                  System.out.println("Give me a  " + word.charAt(i) +": " + word.charAt(i) +"!");
                 }
                 else{
                         System.out.println("Give me an " + word.charAt(i) +": " + word.charAt(i) +"!");
