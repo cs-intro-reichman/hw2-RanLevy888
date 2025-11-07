8947c548889b527c4c4e0c7e965d1b9e68141050
Ran Levy <Ran.levy02@post.runi.ac.il>
2025-11-07 11:57:54 +0200

    fixedddd

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,21 @@
-//feedback
 
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0].toUpperCase();
+            int n = Integer.parseInt(args[1]);
+            String nik = "AEFHILMNORSX";
+            for(int i = 0 ; i < word.length() ; i++ ){
+                if(nik.indexOf(word.charAt(i))==-1){
+                  System.out.println("Give me a " + word.charAt(i) +": " + word.charAt(i) +"!");
+                }
+                else{
+                        System.out.println("Give me an " + word.charAt(i) +": " + word.charAt(i) +"!");
+                }
+      
+            }
+ System.out.println("What does that spell?");
+         for(int i = 0; i <= n ; i++){
+                System.out.println(word + "!!!");
+         }
         }
 }
