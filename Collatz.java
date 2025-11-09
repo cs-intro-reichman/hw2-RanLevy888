8a103ea28f0d5e27ee0d9a6649b1430ace0b0fbe
Ran Levy <Ran.levy02@post.runi.ac.il>
2025-11-09 20:32:07 +0200

    yay

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,33 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	int n = Integer.parseInt(args[0]);
+	String tp = args[1];
+	 int seed = 1;
+     int temp =0;
+	 int counter =1;
+	 if(tp.equals("v")){
+	for(int i = 1 ; i <= n ; i++  ){
+		seed =i;
+		System.out.print(seed);
+		while(temp!=1){
+			if(seed%2==0){
+				seed=seed/2;
+			System.out.print(" " + seed);
+			}
+			else{
+				seed =seed*3+1;
+			System.out.print(" "+ seed);
+			}
+           counter++;
+		   temp = seed;
+		}
+		System.out.print(" (" + counter +")");
+		counter=1;
+           System.out.println( " ");
+		   temp =0;
+	}
+	 }
+System.out.println("Every one of the first " + n + " hailstone sequences reached 1");
 	}
 }
